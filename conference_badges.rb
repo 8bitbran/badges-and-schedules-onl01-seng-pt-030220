20cc32a0cf36b2634cab2c9dd6f706eda35298f3
Brandon Marrero <brandonmar97@gmail.com>
2020-03-09 22:59:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end  
+
+def batch_badge_creator(names)
+  greetings = []
+  names.each do |name|
+    greetings << badge_maker(name)
+  end
+  
+  return greetings
+end
+
+def assign_rooms(speakers)
+  greeting = []
+  speakers.each_with_index {|name, index| greeting << "Hello, #{name}! You'll be assigned to room #{index}!"}
+  greeting
+end 
